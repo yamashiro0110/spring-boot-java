b1a00285c7b08735714051e81379d9add3f24d21
yamashiro0110 <yamashiro0110@gmail.com>
2020-09-24 20:05:22 +0900

    Delete ToDoRepository.java

--- domain/src/main/java/org/yamashiro0110/sample/domain/todo/ToDoRepository.java
+++
@@ -1,8 +0,0 @@
-package org.yamashiro0110.sample.domain.todo;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(path = "todo")
-public interface ToDoRepository extends CrudRepository<ToDo, Long> {
-}
