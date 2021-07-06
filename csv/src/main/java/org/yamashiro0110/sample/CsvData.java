55fb70ee293949d4368c64205c863b2ad65165ac
yamashiro0110 <yamashiro0110@gmail.com>
2021-07-06 23:05:04 +0900

    Update CsvData.java

--- csv/src/main/java/org/yamashiro0110/sample/CsvData.java
+++ csv/src/main/java/org/yamashiro0110/sample/CsvData.java
@@ -1,5 +1,7 @@
 package org.yamashiro0110.sample;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +15,10 @@ public class CsvData {
     private Integer number;
     private String uuid = UUID.randomUUID().toString();
     private String createdAt = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    @JsonIgnore
+    private String ignoreField = "ignore";
+    @JsonProperty("alias")
+    private String aliasField = "alias";
 
     public CsvData(final int number) {
         this.number = number;
